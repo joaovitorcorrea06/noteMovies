1c08ec5f0440e0f3677526fc9ad8f1b5ea52ef51
João Vitor Corrêa <joaov1997@hotmail.com>
2023-05-11 11:33:13 -0300

    .

--- src/components/FilmeSugestao/index.jsx
+++ src/components/FilmeSugestao/index.jsx
@@ -7,13 +7,17 @@ const FilmeSugestao = ({ poster, name, year, duration, platforms }) => {
   return (
     <div className="movie">
       <img src={poster} alt={name} className="poster" />
-      <h3 style={{color: '#fff'}}>{name}</h3>
-      <p style={{color: '#fff'}}>{year}</p>
-      <p style={{color: '#fff'}}>{duration}</p>
-      <div className="platforms">
-        {platforms.map((platform, index) => (
-          <img key={index} src={platform}/>
-        ))}
+      <div className="movieData">
+
+        <h3 style={{color: '#fff'}}>{name}</h3>
+        <p style={{color: '#fff'}}>{year}</p>
+        <p style={{color: '#fff'}}>{duration}</p>
+        <div className="platforms">
+          {platforms.map((platform, index) => (
+            <img key={index} src={platform}/>
+            ))}
+        </div>
+      
       </div>
     </div>
   );
